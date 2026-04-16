8fa28bf6ec54de844085d298288d9001ae407a12
r68720282-afk <r68720282@gmail.com>
2026-04-17 00:48:52 +0530

    Create main.js

---
+++ server/public/js/main.js
@@ -0,0 +1,8 @@
+function openForm(course){
+  document.getElementById("popup").style.display = "block";
+  document.getElementById("course").value = course;
+}
+
+function closeForm(){
+  document.getElementById("popup").style.display = "none";
+}
