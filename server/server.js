657568b0df31dd6649283fdac546e68251ddb1e4
r68720282-afk <r68720282@gmail.com>
2026-04-16 22:06:57 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,15 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
-const express=require("express");
-const mongoose=require("mongoose");
-const cors=require("cors");
-const app=express();
+const app = express();
+
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect("mongodb://127.0.0.1:27017/collegeDB");
+// MongoDB Atlas connection
+mongoose.connect(process.env.MONGO_URI)
+.then(() => console.log("DB Connected"))
+.catch(err => console.log(err));
 
-app.use("/api/visitors",require("./routes/visitors"));
-app.use("/api/users",require("./routes/users"));
-app.use("/api/colleges",require("./routes/colleges"));
+// Routes
+app.use("/api/visitors", require("./routes/visitors"));
+app.use("/api/users", require("./routes/users"));
+app.use("/api/colleges", require("./routes/colleges"));
 
-app.listen(5000);
+// PORT FIX (IMPORTANT)
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port " + PORT);
+});
