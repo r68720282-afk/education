242a1c29a8bb46a86c5b054ce81543d3aaaa9e40
r68720282-afk <r68720282@gmail.com>
2026-04-16 22:01:46 +0530

    Refactor fetch and filter logic in category.js

--- client/js/category.js
+++ client/js/category.js
@@ -1,8 +1,32 @@
-
 fetch("http://localhost:5000/api/colleges")
-.then(r=>r.json())
-.then(d=>{
-const t=new URLSearchParams(location.search).get("type");
-document.getElementById("list").innerHTML=
-d.filter(c=>c.category===t).map(c=>"<p>"+c.name+"</p>").join("");
+.then(res => res.json())
+.then(data => {
+
+  const params = new URLSearchParams(window.location.search);
+  const type = params.get("type");
+  const search = params.get("search");
+
+  let filtered = data;
+
+  if(type){
+    filtered = data.filter(c => c.category === type);
+  }
+
+  if(search){
+    filtered = data.filter(c =>
+      c.name.toLowerCase().includes(search.toLowerCase())
+    );
+  }
+
+  let output = "";
+
+  filtered.forEach(c => {
+    output += `
+      <div class="card">
+        <h3>${c.name}</h3>
+      </div>
+    `;
+  });
+
+  document.getElementById("list").innerHTML = output;
 });
