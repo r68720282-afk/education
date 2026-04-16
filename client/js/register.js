d3ff5a6a34db59171a87676dc0ea4236b19097c2
r68720282-afk <r68720282@gmail.com>
2026-04-16 22:02:49 +0530

    Update register.js

--- client/js/register.js
+++ client/js/register.js
@@ -1,13 +1,14 @@
+document.getElementById("form").onsubmit = async (e)=>{
+  e.preventDefault();
 
-document.getElementById("f").onsubmit=async e=>{
-e.preventDefault();
-await fetch("http://localhost:5000/api/users",{
-method:"POST",
-headers:{"Content-Type":"application/json"},
-body:JSON.stringify({
-name:document.getElementById("name").value,
-email:document.getElementById("email").value
-})
-});
-alert("Saved");
+  await fetch("http://localhost:5000/api/users",{
+    method:"POST",
+    headers:{ "Content-Type":"application/json" },
+    body: JSON.stringify({
+      name: document.getElementById("name").value,
+      email: document.getElementById("email").value
+    })
+  });
+
+  alert("Registered!");
 };
