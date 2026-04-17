1097689382352c6fdf32ac238ee462d0033ffed5
r68720282-afk <r68720282@gmail.com>
2026-04-17 22:17:26 +0530

    Update image URLs and course lists in categories

--- server/public/js/main.js
+++ server/public/js/main.js
@@ -4,22 +4,20 @@ const categories={
 Medical:{
 title:"Nursing Courses",
 desc:"Best nursing programs",
-img:"https://images.unsplash.com/photo-1588776814546-ec7e0f0f9c1b?w=400",
-courses:["GNM","BSc Nursing","ANM"]
+img:"https://images.unsplash.com/photo-1588776814546-ec7e0f0f9c1b?w=600",
+courses:["GNM","BSc Nursing"]
 },
-
 Engineering:{
 title:"Engineering Courses",
 desc:"Top engineering programs",
-img:"https://images.unsplash.com/photo-1581092580497-e0d23cbdf1dc?w=400",
-courses:["B.Tech","Diploma"]
+img:"https://images.unsplash.com/photo-1581092580497-e0d23cbdf1dc?w=600",
+courses:["B.Tech"]
 },
-
 Computer:{
 title:"IT Courses",
 desc:"IT programs",
-img:"https://images.unsplash.com/photo-1518770660439-4636190af475?w=400",
-courses:["BCA","MCA"]
+img:"https://images.unsplash.com/photo-1518770660439-4636190af475?w=600",
+courses:["BCA"]
 }
 };
 
@@ -35,8 +33,6 @@ catImage.src=data.img;
 
 courseList.innerHTML=
 data.courses.map(c=>`<span onclick="openForm('${c}')">${c}</span>`).join("");
-
-window.scrollTo({top:500,behavior:"smooth"});
 }
 
 function openForm(course){
